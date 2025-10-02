9b0eeb073332781c405514babbc5ecd35b441c23
Justin Finn <122637243+Justinfinn2891@users.noreply.github.com>
2025-10-02 18:24:07 -0500

    Updating motor.h to support functions, commented out old code

--- 2dMapping/LidarHardware/include/motor.h
+++ 2dMapping/LidarHardware/include/motor.h
@@ -7,11 +7,12 @@
 #include <unistd.h> // Need for sleep functions 
 #include <iostream>
 
-const int NUM_X = 8;
-const int NUM_Y = 4;
+//const int NUM_X = 8;
+//const int NUM_Y = 4; 
 class Motor{
 
     private:
+    /*
         int steps[NUM_X][NUM_Y] = {
         {1, 0, 0, 0},
         {1, 1, 0, 0},
@@ -23,18 +24,28 @@ class Motor{
         {1, 0, 0, 1}
     };
         
-
+    */
     public:
-  
-        Motor();
+        int STEP_PIN;
+        int DIR_PIN;
+        int ENA_PIN;
+        Motor(int stepPin, int dirPin, int enaPin = -1);
+        void Activate();
+        void Deactivate();
+        void forward(int steps, int delayMs);
+        void backward(int steps, int delayMs);
 
-        int Pin1, Pin2, Pin3, Pin4;
+        //int Pin1, Pin2, Pin3, Pin4;
         // FUNCTIONS
+
+/*
         void stepMotor(const int &step);
         void forward(const int &stepCount, const int &delayMs = 2);
         void backward(const int &stepCount, const int &delayMs = 2);
         void Activate();
         void Deactivate();
+*/
 };
+
 
 #endif // MOTOR_H
